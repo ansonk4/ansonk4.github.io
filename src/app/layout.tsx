a820d4954f9505293bd5ede70908cfef028b935e
ansonk4 <kwoktsunon@yahoo.com>
2026-02-14 18:31:43 +0800

    Adjust metadata description text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Tsun On Kwok",
-  description: "Tsun On Kwok.",
+  description: "Tsun On Kwok",
   icons: {
     icon: "/ust.svg",
   },

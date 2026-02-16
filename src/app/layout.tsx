5a72fe7cdc045019fa812dd31712aa27c9c4e109
ansonk4 <kwoktsunon@yahoo.com>
2026-02-16 18:21:32 +0800

    Revert "Force maximum default zoom-out on mobile"
    This reverts commit 7b8ec1381a331b675236d63a113c28c5c3b906f3.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,10 +12,7 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   width: 1200,
-  initialScale: 0.1,
-  minimumScale: 0.1,
-  maximumScale: 5,
-  userScalable: true,
+  initialScale: 0.33,
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {

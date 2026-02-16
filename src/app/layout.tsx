dd736fc689c98596736580586ed4e82dc3a7d347
ansonk4 <kwoktsunon@yahoo.com>
2026-02-16 18:21:32 +0800

    Revert "Set desktop-width viewport with default zoom-out on mobile"
    This reverts commit 07c2805d47b11f90c439dbcc2abd508234f9480e.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  width: 1200,
-  initialScale: 0.33,
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
